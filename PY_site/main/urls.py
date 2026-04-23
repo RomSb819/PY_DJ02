91e9b8ecb819b7c46a2c4eb3090356206287da8e
RomSb819 <technika.acc@gmail.com>
2026-04-23 23:42:28 +0300

    Eco_site_done

--- PY_site/main/urls.py
+++ PY_site/main/urls.py
@@ -4,6 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('new/', views.new, name='page2'),
+    path("", views.index, name="home"),
+    path("new/", views.new, name="problems"),
+    path("help/", views.help_nature, name="help_nature"),
+    path("about/", views.about, name="about"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- PY_site/main/views.py
+++ PY_site/main/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 
 
-# Create your views here.
-from django.http import HttpResponse
-
 def index(request):
-    return render(request, 'main/index.html', {'caption': "Берегите природу"})
+    return render(request, "main/index.html")
+
 
 def new(request):
-    return render(request, 'main/new.html')
+    return render(request, "main/new.html")
+
+
+def help_nature(request):
+    return render(request, "main/help_nature.html")
+
+
+def about(request):
+    return render(request, "main/about.html")
